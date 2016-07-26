32228858644de23b2589c182c238a29006c7bddd
Amaury Martiny <amaury.martiny@gmail.com>
2016-07-26 11:09:52 +0200

    Use long lived connection for message passing

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,13 +1,56 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+var trackerTimer = null; // 1s timer that tracks time spent on facebook
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+var timeTrackedToday = 0;
+var timeTrackedTotal = 0;
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+// chrome.runtime.onInstalled.addListener(details => {
+//     console.log('previousVersion', details.previousVersion);
+// });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request.action);
+/**
+ * Listen to messages from the content scripts
+ */
+chrome.runtime.onConnect.addListener(port => {
+
+  //
+  console.assert(port.name == 'trackingTime');
+
+  /**
+   * Start tracking time spent on facebook
+   */
+  function startTrackerTimer() {
+    if (trackerTimer) return;
+    trackerTimer = setInterval(updateTime, 1000);
+  }
+
+  /**
+   * Stop tracking time spent on facebook
+   */
+  function stopTrackerTimer() {
+    clearInterval(trackerTimer);
+    trackerTimer = null;
+  }
+
+  /**
+   * Update the time tracked
+   */
+  function updateTime() {
+    ++timeTrackedToday;
+    ++timeTrackedTotal;
+    port.postMessage({
+      action: 'updateTrackedTime',
+      timeTrackedToday: timeTrackedToday,
+      timeTrackedTotal: timeTrackedTotal
+    });
+  }
+
+  port.onMessage.addListener(msg => {
+    console.log(msg.action)
+    if (msg.action == 'startTrackingTime')
+      startTrackerTimer();
+    if (msg.action == 'stopTrackingTime')
+      stopTrackerTimer();
+  });
 });
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,8 +2,11 @@
 
 var idleTimer; // the 5s timer that checks idleness of user
 var isTrackingTime = false; // variable to check if app is currently tracking time spent on facebook
+var trackingTimePort = chrome.runtime.connect({name: 'trackingTime'}); // start a long-lived connection with background for time tracking
 
-// reset idleness on the following events;
+/**
+ * Reset idleness on the following events
+ */
 window.addEventListener('load', resetIdleTimer, false);
 window.addEventListener('mousemove', resetIdleTimer, false);
 window.addEventListener('mousedown', resetIdleTimer, false);
@@ -13,35 +16,45 @@ window.addEventListener('mousewheel', resetIdleTimer, false);
 window.addEventListener('touchmove', resetIdleTimer, false);
 window.addEventListener('MSPointerMove', resetIdleTimer, false);
 
-// stop counting time when the page closes
+/**
+ * Stop counting time when the page closes
+ */
 window.addEventListener('beforeunload', stopTrackingTime, false);
 
 /**
  * Resets the idle timer, triggered when a user event (click, mousemove) is detected
  */
 function resetIdleTimer() {
-    clearTimeout(idleTimer);
-    startTrackingTime();
-    idleTimer = setTimeout(stopTrackingTime, 5000); // 1000 millisec = 1 sec
+  clearTimeout(idleTimer);
+  startTrackingTime();
+  idleTimer = setTimeout(stopTrackingTime, 5000); // 1000 millisec = 1 sec
 };
 
 /**
  * Sends a message to background to start tracking time
  */
 function startTrackingTime() {
-    if (isTrackingTime) return; // if we're already tracking time then do nothing
-    isTrackingTime = true;
-    chrome.runtime.sendMessage({action: 'startTrackingTime'}, function(response) {});
-    console.log('start tracking time');
+  if (isTrackingTime) return; // if we're already tracking time then do nothing
+  isTrackingTime = true;
+  // chrome.runtime.sendMessage({action: 'startTrackingTime'}, function(response) {});
+  trackingTimePort.postMessage({action: 'startTrackingTime'});
+  console.log('start tracking time');
 }
 
 /**
  * Sends a message to background to stop tracking time
  */
 function stopTrackingTime() {
-    isTrackingTime = false;
-    chrome.runtime.sendMessage({action: 'stopTrackingTime'}, function(response) {});
-    console.log('stop tracking time');
+  isTrackingTime = false;
+  trackingTimePort.postMessage({action: 'stopTrackingTime'});
+  // chrome.runtime.sendMessage({action: 'stopTrackingTime'}, function(response) {});
+  console.log('stop tracking time');
 }
 
-console.log('\'Allo \'Allo! Content script');
+/**
+ * Update time on facebook page when receiving a message from background
+ */
+trackingTimePort.onMessage.addListener(msg => {
+  if (msg.action == 'updateTrackedTime')
+    console.log(msg);
+});
