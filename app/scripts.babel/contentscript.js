8ce4cd4edc981f2dba6a49b74a1730b765d2cafb
Amaury Martiny <amaury.martiny@gmail.com>
2016-07-26 09:51:09 +0200

    Detect idleness on page

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,19 @@
 'use strict';
 
+var idleTimer; // the 5s timer that checks idleness
+window.onload = resetTimer;
+document.onmousemove = resetTimer;
+document.onkeypress = resetTimer;
+
+function logout() {
+    console.log('You are now logged out.');
+    //location.href = 'logout.php'
+};
+
+function resetTimer() {
+    console.log('Realoding timer');
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(logout, 5000); // 1000 millisec = 1 sec
+};
+
 console.log('\'Allo \'Allo! Content script');
