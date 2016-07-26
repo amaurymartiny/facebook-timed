21dbc4fccc5c707ab18d33b35b559777ab18abe0
Amaury Martiny <amaury.martiny@gmail.com>
2016-07-26 21:59:50 +0200

    Multiple long lived connections

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -2,50 +2,54 @@
 
 var trackerTimer = null; // 1s timer that tracks time spent on facebook
 
-var timeTrackedToday = 0;
-var timeTrackedTotal = 0;
+var timeTrackedToday = 0; // time spent on facebook today
+var timeTrackedTotal = 0; // time spent on facebook all time
 
-// chrome.runtime.onInstalled.addListener(details => {
-//     console.log('previousVersion', details.previousVersion);
-// });
+var ports = []; // when multiple facebook tabs, save all the long-lived connection port inside this array
 
 /**
- * Listen to messages from the content scripts
+ * Start tracking time spent on facebook
  */
-chrome.runtime.onConnect.addListener(port => {
+function startTrackerTimer() {
+  if (trackerTimer) return;
+  trackerTimer = setInterval(updateTime, 1000);
+}
 
-  //
-  console.assert(port.name == 'trackingTime');
+/**
+ * Stop tracking time spent on facebook
+ */
+function stopTrackerTimer() {
+  clearInterval(trackerTimer);
+  trackerTimer = null;
+}
 
-  /**
-   * Start tracking time spent on facebook
-   */
-  function startTrackerTimer() {
-    if (trackerTimer) return;
-    trackerTimer = setInterval(updateTime, 1000);
-  }
-
-  /**
-   * Stop tracking time spent on facebook
-   */
-  function stopTrackerTimer() {
-    clearInterval(trackerTimer);
-    trackerTimer = null;
-  }
-
-  /**
-   * Update the time tracked
-   */
-  function updateTime() {
-    ++timeTrackedToday;
-    ++timeTrackedTotal;
-    port.postMessage({
+/**
+ * Update time on content scripts label to be shown on Facebook DOM
+ * @param  {Port} ports all the opened long-lived connections to send the time to
+ */
+function updateTime() {
+  ++timeTrackedToday;
+  ++timeTrackedTotal;
+  console.log(ports);
+  for (var i = ports.length - 1; i >= 0; i--) {
+    ports[i].postMessage({
       action: 'updateTrackedTime',
       timeTrackedToday: timeTrackedToday,
       timeTrackedTotal: timeTrackedTotal
     });
   }
+}
 
+/**
+ * Run when a new long-lived connection is established (i.e. a new Facebook tab opens)
+ */
+chrome.runtime.onConnect.addListener(port => {
+
+  // add this port to the list of all ports
+  console.log('New port opened.');
+  ports.push(port);
+
+  // listen to messages from the content scripts
   port.onMessage.addListener(msg => {
     console.log(msg.action)
     if (msg.action == 'startTrackingTime')
@@ -53,4 +57,14 @@ chrome.runtime.onConnect.addListener(port => {
     if (msg.action == 'stopTrackingTime')
       stopTrackerTimer();
   });
+
+  port.onDisconnect.addListener(port => {
+    for (var i = ports.length - 1; i >= 0; i--) {
+      if (ports[i].sender.id == port.sender.id) {
+        ports.splice(i, 1);
+        console.log('Port closed.');
+        break;
+      }
+    }
+  })
 });
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,6 +3,7 @@
 var idleTimer; // the 5s timer that checks idleness of user
 var isTrackingTime = false; // variable to check if app is currently tracking time spent on facebook
 var trackingTimePort = chrome.runtime.connect({name: 'trackingTime'}); // start a long-lived connection with background for time tracking
+var timeLabel; // DOM element that contains the time spent on facebook
 
 /**
  * Reset idleness on the following events
@@ -17,9 +18,24 @@ window.addEventListener('touchmove', resetIdleTimer, false);
 window.addEventListener('MSPointerMove', resetIdleTimer, false);
 
 /**
- * Stop counting time when the page closes
+ * Stop counting time when the page closes and disconnect port
  */
-window.addEventListener('beforeunload', stopTrackingTime, false);
+window.addEventListener('beforeunload', () => {
+  stopTrackingTime();
+  trackingTimePort.disconnect();
+}, false);
+
+/**
+ * Modify Facebook DOM to add label with time tracked
+ */
+(function() {
+  var el = document.querySelector('[role="navigation"]');
+  timeLabel = document.createElement('li');
+  timeLabel.id = 'facebook-timed-label';
+  timeLabel.innerHTML = '00:00:00';
+  el.insertBefore(timeLabel, el.firstChild);
+  console.log('done!')
+})();
 
 /**
  * Resets the idle timer, triggered when a user event (click, mousemove) is detected
@@ -56,5 +72,17 @@ function stopTrackingTime() {
  */
 trackingTimePort.onMessage.addListener(msg => {
   if (msg.action == 'updateTrackedTime')
-    console.log(msg);
+    updateLabel(msg.timeTrackedToday);
 });
+
+/**
+ * Update time shown on label with updated value
+ */
+function updateLabel(timeTrackedToday) {
+  // update time label with hh:mm:ss format
+  var hours = parseInt(timeTrackedToday / 3600) % 24;
+  var minutes = parseInt(timeTrackedToday / 60) % 60;
+  var seconds = timeTrackedToday % 60;
+  var hhmmss = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds  < 10 ? '0' + seconds : seconds);
+  timeLabel.innerHTML = hhmmss;
+}
