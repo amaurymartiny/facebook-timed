37590e911746fc8c2badaacd255c24367698b28c
Amaury Martiny <amaury.martiny@gmail.com>
2016-07-28 18:13:38 +0200

    Change name from FacebookTimed to Timed

--- extension/gulpfile.babel.js
+++ extension/gulpfile.babel.js
@@ -119,7 +119,7 @@ gulp.task('wiredep', () => {
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
   return gulp.src('dist/**')
-      .pipe($.zip('FacebookTimed-' + manifest.version + '.zip'))
+      .pipe($.zip('Timed-' + manifest.version + '.zip'))
       .pipe(gulp.dest('package'));
 });
 
