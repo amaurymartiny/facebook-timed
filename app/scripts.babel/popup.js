cdebb59fb044476ced5e7b7a2eb27d73839ad77b
Amaury Martiny <amaury.martiny@gmail.com>
2016-07-28 18:06:34 +0200

    Change design of popup

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -15,12 +15,48 @@ var installDateLabel = document.getElementById('install-date-label');
  */
 trackingTimePort.onMessage.addListener(msg => {
   if (msg.action == 'updateTrackedTime') {
-  	console.log(msg)
-  	timeTrackedTodayLabel.innerHTML = msg.timeTrackedToday;
-  	timeTrackedTotalLabel.innerHTML = msg.timeTrackedTotal;
-  	installDateLabel.innerHTML = msg.installDate;
+    timeTrackedTodayLabel.innerHTML = readableTime(msg.timeTrackedToday);
+    timeTrackedTotalLabel.innerHTML = readableTime(msg.timeTrackedTotal);
+    installDateLabel.innerHTML = new Date(msg.installDate);
   }
     
 });
 
-console.log('\'Allo \'Allo! Popup');
+/**
+ * Transform seconds to hh:mm:ss format
+ */
+function readableTime(totalSec) {
+  var hours = parseInt( totalSec / 3600 ) % 24;
+  var minutes = parseInt( totalSec / 60 ) % 60;
+  var seconds = totalSec % 60;
+
+  return '<span class="time-tracked-number">' + (hours < 10 ? '0' + hours : hours) + '</span>h<span class="time-tracked-number">' + (minutes < 10 ? '0' + minutes : minutes) + '</span>m<span class="time-tracked-number">' + (seconds  < 10 ? '0' + seconds : seconds) + '</span>s';
+}
+
+// // Decode utf8 characters properly
+// const cid = 'fDytCVJ9hSD0jNdXj5mgibrbbFF3AyAJ';
+// const domain = 'amaurymartiny.auth0.com';
+
+// const lock = new Auth0Lock(cid, domain);
+
+// lock.show();
+
+// var connection = config.connection;
+// var prompt = config.prompt;
+
+// var initializationOptions = {
+//   assetsUrl:  config.assetsUrl,
+//   cdn:        config.cdn
+// };
+
+// var lock = new Auth0Lock(config.clientID, config.auth0Domain, initializationOptions);
+// lock.show({
+//   // icon:            '{YOUR_LOGO_URL}',
+//   callbackURL:        config.callbackURL,
+//   responseType:       config.callbackOnLocationHash ? 'token' : 'code',
+//   dict:               config.dict,
+//   connections:        connection ? [connection] : null,
+//   rememberLastLogin:  !prompt,
+//   container:          'widget-container',
+//   authParams:         config.internalOptions
+// });
