2f4bbe36a5ef33694026f736f6e074965bb1914f
Amaury Martiny <amaury.martiny@gmail.com>
2016-10-13 11:09:25 +0200

    Change default port and remove history package

--- app/config/main.js
+++ app/config/main.js
@@ -3,22 +3,8 @@
 var config = {
   env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 8889,
-  karmaPort: 9876,
-
-  // This part goes to React-Helmet for Head of our HTML
-  app: {
-    head: {
-      title: 'barbar-vortigern',
-      titleTemplate: 'barbar-vortigern: %s',
-      meta: [
-        { charset: 'utf-8' },
-        { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'React Redux Typescript' },
-      ]
-    }
-  }
+  port: process.env.PORT || 3000,
+  karmaPort: 9876
 };
 
 module.exports = config;
