ee9b4b549ffef5a9d7514d473029da738bbac763
Amaury Martiny <amaury.martiny@gmail.com>
2016-10-17 20:01:45 +0200

    Remove all previous files

--- app/src/config.tsx
+++
@@ -1,8 +0,0 @@
-export const ENV = process.env.NODE_ENV || 'development';
-export const HOST = process.env.HOST || 'localhost';
-export const PORT = process.env.PORT || 3000;
-export const AUTH0_DOMAIN = 'something.auth0.com';
-export const AUTH0_CLIENT_ID = 'put your own client id';
-export const AUTH0_CLIENT_SECRET = 'put your own client secret';
-export const AUTH0_CALLBACK_URL = 'http://localhost:3000/callback';
-export const KARMA_PORT = 9876;
