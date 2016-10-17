0998a6a1637522653ea702437e9761db11bd594e
Amaury Martiny <amaury.martiny@gmail.com>
2016-10-17 18:26:00 +0200

    Remove stars component

--- app/src/app/components/Header/index.tsx
+++ app/src/app/components/Header/index.tsx
@@ -8,9 +8,8 @@ class Header extends React.Component<any, any> {
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="about">About</Link></li>
+          <li><Link to="about">Absout</Link></li>
           <li><Link to="counter">Counter</Link></li>
-          <li><Link to="stars">Stars</Link></li>
         </ul>
       </nav>
     );
