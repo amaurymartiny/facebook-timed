1a09c0f58d935a2e7687ca777ae199d2e5f97205
Amaury Martiny <amaury.martiny@gmail.com>
2016-10-19 19:27:43 +0200

    Add missing package

--- app/client/app/components/Header.js
+++ app/client/app/components/Header.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import styles from './Header.css';
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={styles.app}>
-        <h1>Timed</h1>
-      </div>
-    );
-  }
-}
+const Header = () =>
+  <div className={styles.app}>
+    <h1>Time</h1>
+  </div>;
+
+export default Header;
