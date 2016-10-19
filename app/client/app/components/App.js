7f2e982e22dd1e973f658315a188a7df5ae658e7
Amaury Martiny <amaury.martiny@gmail.com>
2016-10-19 19:01:23 +0200

    Add auth0 lock

--- app/client/app/components/App.js
+++ app/client/app/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Header from './Header';
+import LoginPage from './LoginPage';
+import AuthService from '../utils/AuthService';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,11 +10,13 @@ export default class App extends React.Component {
     this.state = {test: 'foo'};
   }
   render() {
+    const auth = new AuthService('SyZVm6XmXC4JgIBfw1sj3iHTEdmJ59UC', 'timed.auth0.com');
     return (
       <div>
         <Header />
         bar
         <input type="text" />
+        <LoginPage auth={auth} />
       </div>
     );
   }
