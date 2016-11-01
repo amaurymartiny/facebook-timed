3120cdd0f2e1065f3985877acd2d353382f9d2d2
Amaury Martiny <amaury.martiny@gmail.com>
2016-11-01 17:41:51 +0100

    Add website key in iterator

--- app/client/src/components/TrackList/TrackList.js
+++ app/client/src/components/TrackList/TrackList.js
@@ -5,7 +5,8 @@ const TrackList = ({ tracks }) =>
     <h3>Tracked websites ({tracks.length}):</h3>
     <ul>
       {tracks.map(track =>
-        <li>
+        <li key={track.website._id}>
+          <a href={track.website.url} target="_blank"><h4>{track.website.name}</h4></a>
           <p>Today: {track.timeSpentToday}</p>
           <p>Total: {track.timeSpentTotal}</p>
         </li>
