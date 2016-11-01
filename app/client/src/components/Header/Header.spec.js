704ee171845f3d96dfe0c883e540839c8cc56c52
Amaury Martiny <amaury.martiny@gmail.com>
2016-11-01 01:58:22 +0100

    Add tests

--- app/client/src/components/Header/Header.spec.js
+++ app/client/src/components/Header/Header.spec.js
@@ -6,6 +6,6 @@ import Header from './Header'
 describe('(Component) <Header />', () => {
   it('should render the correct title', () => {
     const wrapper = shallow(<Header isAuthenticated={false} onLoginClick={()=>{}} onLogoutClick={()=>{}} />)
-    expect(wrapper.find('h1').text()).to.equal('React Redux Auth0 Kit')
+    expect(wrapper.find('h1').text()).to.equal('Timed')
   })
 })
