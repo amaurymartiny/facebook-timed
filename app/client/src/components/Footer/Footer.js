9b07ef111e8b4dde916ecf75f0039d34ca348dd7
Amaury Martiny <amaury.martiny@gmail.com>
2016-11-14 15:59:08 +0800

    Wording

--- app/client/src/components/Footer/Footer.js
+++ app/client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.less'
 const Footer = () =>
   <footer className="text-center">
     Made with <Glyphicon glyph="heart" /> by Amaury Martiny. 
-    Send me <a href="mailto:amaury.martiny@gmail.com">an email</a> to say hi.
+    Send <a href="mailto:amaury.martiny@gmail.com">an email</a> to say hi.
   </footer>
 
 export default Footer
