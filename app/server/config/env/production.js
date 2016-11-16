df0b912de5600f2bfedb3322d9ad1b6ae8e3841b
Amaury Martiny <amaury.martiny@gmail.com>
2016-11-17 06:46:18 +0800

    Use environment variables for production

--- app/server/config/env/production.js
+++ app/server/config/env/production.js
@@ -1,6 +1,7 @@
 export default {
   env: 'production',
-  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://localhost/timed',
-  port: 8080
+  jwtSecret: process.env.JWT_SECRET,
+  jwtAudience: process.env.JWT_AUDIENCE,
+  db: process.env.MONGODB_URI,
+  port: process.env.PORT || 8080
 };
