0f07febe07e9d3919fa7f44543b819a483eba3da
Amaury Martiny <amaury.martiny@gmail.com>
2016-11-16 16:43:26 +0800

    Add icon image on header

--- app/client/src/components/Header/Header.jsx
+++ app/client/src/components/Header/Header.jsx
@@ -3,12 +3,14 @@ import Navbar from 'react-bootstrap/lib/Navbar'
 import Nav from 'react-bootstrap/lib/Nav'
 import { Link } from 'react-router'
 import { LoginContainer } from '../../containers'
+import brandLogo from '../../../assets/watch_white.svg'
+import './Header.less'
 
 const Header = () =>
   <Navbar collapseOnSelect fluid>
     <Navbar.Header>
       <Link to="/">
-        <Navbar.Brand>Timed</Navbar.Brand>
+        <Navbar.Brand><img src={brandLogo} alt="brand-logo" />Timed</Navbar.Brand>
       </Link>
       {/* <Navbar.Toggle />*/}
     </Navbar.Header>
