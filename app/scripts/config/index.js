5d3d9488886faea9df573c56d6262687b438db78
Amaury Martiny <amaury.martiny@gmail.com>
2017-09-11 01:33:44 +0200

    Add twitter

--- app/scripts/config/index.js
+++ app/scripts/config/index.js
@@ -1,8 +1,10 @@
 import facebook from './websites/facebook';
 import reddit from './websites/reddit';
+import twitter from './websites/twitter';
 
 // List of websites that are trackable
 export const trackableWebsites = {
   facebook,
-  reddit
+  reddit,
+  twitter
 };
---
+++ app/scripts/config/websites/twitter.js
@@ -0,0 +1,13 @@
+export default {
+  regex: /(?:www.)?twitter.com/,
+  addElement: () => {
+    const el = document.querySelector('.global-nav .nav-extras');
+    // Add the label containing the tracked time
+    const label = document.createElement('span');
+    label.className = 'timed-label-twitter';
+    label.innerHTML = '00:00:00';
+    el.insertBefore(label, el.firstChild);
+    // Important! Return the label element
+    return label;
+  }
+};
