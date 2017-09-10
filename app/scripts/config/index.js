240f5e3bcbb13f6968594691d622a8853e0e5d07
Amaury Martiny <amaury.martiny@gmail.com>
2017-09-11 01:45:59 +0200

    Add 9gag

--- app/scripts/config/index.js
+++ app/scripts/config/index.js
@@ -1,10 +1,12 @@
 import facebook from './websites/facebook';
+import ninegag from './websites/9gag';
 import reddit from './websites/reddit';
 import twitter from './websites/twitter';
 
 // List of websites that are trackable
 export const trackableWebsites = {
   facebook,
+  '9gag': ninegag,
   reddit,
   twitter
 };
---
+++ app/scripts/config/websites/9gag.js
@@ -0,0 +1,15 @@
+export default {
+  name: '9GAG',
+  regex: /(?:www.)?9gag.com/,
+  addElement: () => {
+    const el = document.querySelector('.nav-menu .menu');
+    const li = document.createElement('li');
+    el.appendChild(li);
+    const label = document.createElement('a');
+    label.className = `timed-label-9gag`;
+    label.innerHTML = '00:00:00';
+    li.appendChild(label);
+    // Important! Return the label element
+    return label;
+  }
+};
